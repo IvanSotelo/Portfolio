84e6e9c946de9cda887c7d7f74d2cfd67827e419
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-24 11:50:46 -0700

    fix: loader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template lang="pug">
-  #app
-    main
-      .message This is a hover effects demo, please view on desktop.
-      h1.page-title.title MY LAST &nbsp;
-        span.slideshow__title__offset.title__offset WORKS
+  #app(v-bind:class="[isLoading ? 'is-loading' : 'is-loaded' , routeName]")
+    Header
+    Loader(v-if="isLoading")
+    router-view(v-else)
 </template>
+
+<script>
+import Loader from '@/components/Loader'
+import Header from '@/components/Header'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+    Loader,
+    Header
+  },
+  computed: {
+    ...mapGetters(['isLoading']),
+    routeName () {
+      return `is-${this.$route.name}`
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import "./style/_utils.scss";
 @import "./style/_reset.scss";
@@ -13,6 +33,22 @@ body {
     font-family: 'source-sans-pro', sans-serif;
     font-size: 1.4rem;
     font-weight: 400;
+}
+
+.ui-splash-in {
+    opacity: 0;
+}
+
+.is-loaded {
+  .ui-splash-in {
+    -webkit-transition: opacity .82s cubic-bezier(.4,.22,.21,1.04);
+    transition: opacity .82s cubic-bezier(.4,.22,.21,1.04);
+    opacity: 1;
+  }
+}
+
+.side {
+  position: fixed;
 }
 
 #scene {
