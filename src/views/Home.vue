972a6a8672067e8d824c328ff9de45f40338827c
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-24 11:51:33 -0700

    fix: home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+main
+  h1.page-title.title MY LAST &nbsp;
+    span.slideshow__title__offset.title__offset WORKS
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
